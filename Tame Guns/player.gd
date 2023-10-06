6459e80dc61091d97fa685ff4e5bd03487fc622f
jdr512 <bemani@gmail.com>
2023-10-06 01:36:58 -0500

    Even more Clint Sprites!
    Changed some logic and added a jumpRate so you can't just spam jump, small landing recovery for no jumping

--- Tame Guns/player.gd
+++ Tame Guns/player.gd
@@ -3,22 +3,24 @@ extends CharacterBody2D
 
 var moveSpeed : float = 65
 var jumpForce : float = 200
-var gravity : float = 700
+var gravity : float = 900
 
 var updateCrossHairPos : Vector2
 var characterVector : Vector2
 
 var bullet = preload ("res://playerbullet.tscn") # Preload the bullet scene so it can spawn
 var canFire = true
+var canJump = true
 var isMoving = false
 var isJumping = false
 
 @onready var _animated_sprite = $AnimatedSprite2D
 
 @export var fireRate = .3
+@export var jumpRate = .6
 
 func _process(_delta):
-	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and canFire: # If the Cooldown for firing is up, fire if left mouse button has been clicked
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and canFire and is_on_floor(): # If the Cooldown for firing is up, fire if left mouse button has been clicked
 		var b = bullet.instantiate() # This creates a copy of the scene definied in var bullet above and thus the script for the bullet
 		owner.add_child(b) # Make a child of root ("Owner")
 		b.transform = $shotSpawn.global_transform # Force the bullet to spawn at shotSpawn marker
@@ -31,7 +33,8 @@ func _physics_process(delta):
 
 	velocity.x = 0
 	isMoving = false
-	
+	print(isJumping)
+		
 	if velocity.y == 0:
 		isJumping = false
 	
@@ -44,12 +47,15 @@ func _physics_process(delta):
 	if Input.is_key_pressed(KEY_D):
 		velocity.x += moveSpeed
 		
-	if Input.is_key_pressed(KEY_SPACE) and is_on_floor():
+	if Input.is_key_pressed(KEY_SPACE) and is_on_floor() and canJump:
 		velocity.y = -jumpForce
-		_animated_sprite.play("Jump-n")
 		isJumping = true
+		canJump = false # Set ability to fire to false, so can't fire
+		await get_tree().create_timer(jumpRate).timeout # This waits to execute the next line. Adjust variable to be able to fire faster.
+		canJump = true # Set fire back to true so can fire again
+		
 
-	print(isJumping)
+
 	
 	if velocity.x == -65 or velocity.x == 65:
 		isMoving = true
@@ -57,15 +63,24 @@ func _physics_process(delta):
 #	if isJumping:
 #		_animated_sprite.play("Jump-n")
 	
+	if velocity.x == -65:
+		_animated_sprite.flip_h = true
+	else:
+		_animated_sprite.flip_h = false
+	
 	if !isMoving and !isJumping:
 		_animated_sprite.play("Idle")
 	
+	if isMoving and isJumping:
+		_animated_sprite.play("Jump")
+		
+	if !isMoving and isJumping:
+		_animated_sprite.play("Jump-n")
+	
 	if isMoving and !isJumping:
 		_animated_sprite.play("Run")
-		if velocity.x == -65:
-			_animated_sprite.flip_h = true
-		else:
-			_animated_sprite.flip_h = false
+
+
 	
 		
 		
